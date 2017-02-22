f913459db902452e2b6af5c0acec6df34e1d64e1
Amith <amith.kumar@gmail.com>
2017-02-23 00:53:23 +0530

    Added Route Params to search hotel

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,8 +2,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { ListPageComponent } from './list-page/list-page.component';
 import {DetailsPageComponent} from './details-page/details-page.component';
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/list', pathMatch: 'full' },
-  { path: 'list', component: ListPageComponent },
+  // { path: '', redirectTo: '/list/:'hotel', pathMatch: 'full' },
+  { path: 'list/:listName', component: ListPageComponent },
   { path: 'details', component: DetailsPageComponent }
 ];
 export const appRoutingProviders: any[] = [];
--- src/app/list-page/list-page.component.ts
+++ src/app/list-page/list-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-list-page',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+
+this.route.params.subscribe(params => {
+       console.log("route data="+params['listName'])
+    });
+
   }
 
 }
