83c8a4b0d12ca8de2dcdcfdad7d5a048473cad50
amith <amithgwl>
2017-03-12 17:27:59 +0530

    menu updation

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -9,6 +9,37 @@ export class NavbarComponent implements OnInit {
 
   constructor() { }
 
+  menuData = [
+    {
+      "menuName": "Hotels",
+      "routeLink": "/list/hotel",
+      "iconClass": "fa fa-h-square",
+      "imageUrl": ""
+    },
+    {
+      "menuName": "Cinima",
+      "routeLink": "/list/cinima",
+      "iconClass": "fa fa-film",
+      "imageUrl": ""
+    },
+    {
+      "menuName": "flower shops",
+      "routeLink": "/list/flower",
+      "iconClass": "fa fa-gift",
+      "imageUrl": ""
+    },
+    {
+      "menuName": "doctors",
+      "routeLink": "/list/doctor",
+      "iconClass": "fa fa-user-md",
+      "imageUrl": ""
+    },
+    {
+      "menuName": "pharma",
+      "routeLink": "/list/pharma",
+      "iconClass": "fa fa-plus-square",
+      "imageUrl": ""
+    }];
   ngOnInit() {
   }
 
--- src/app/mock/itemList.ts
+++ src/app/mock/itemList.ts
@@ -1,250 +1,296 @@
-import { listModel } from '../model/listModel';
+import { listModel } from "../model/listModel";
 export var listData: listModel[] = [
     {
         "Id": "H1",
-        "name": 'hotel 1',
+        "name": "hotel 1",
         "category": "hotel",
         "imageUrl": "System",
         "reviewCount": "15",
         "ratingCount": "4",
         "description": "hotel description 1",
         "address": "Tumkur",
-        "latitude":13.3282036,
-        "longitude":77.1107599
+        "latitude": 13.3282036,
+        "longitude": 77.1107599
     },
     {
         "Id": "H2",
-        "name": 'hotel 2',
+        "name": "hotel 2",
         "category": "hotel",
         "imageUrl": "System",
         "reviewCount": "15",
         "ratingCount": "4",
         "description": "hotel description 2",
-        "address": "Tumkur", 
-        "latitude":13.3282036, "longitude":77.1107599 },   
+        "address": "Tumkur",
+        "latitude": 13.3282036,
+        "longitude": 77.1107599
+    },
     {
         "Id": "H3",
-        "name": 'hotel 3',
+        "name": "hotel 3",
         "category": "hotel",
         "imageUrl": "System",
         "reviewCount": "15",
         "ratingCount": "4",
         "description": "hotel description 3",
-        "address": "Tumkur"
-    , "latitude":13.3282036, "longitude":77.1107599 },   
+        "address": "Tumkur",
+        "latitude": 13.3282036,
+        "longitude": 77.1107599
+    },
     {
         "Id": "H4",
-        "name": 'hotel 4',
+        "name": "hotel 4",
         "category": "hotel",
         "imageUrl": "System",
         "reviewCount": "15",
         "ratingCount": "4",
-        "description": "hotel description 4"
-        , "address": "Tumkur"
-    , "latitude":13.3282036, "longitude":77.1107599 },   
+        "description": "hotel description 4",
+        "address": "Tumkur",
+        "latitude": 13.3282036,
+        "longitude": 77.1107599
+    },
     {
         "Id": "H5",
-        "name": 'hotel 5',
+        "name": "hotel 5",
         "category": "hotel",
         "imageUrl": "System",
         "reviewCount": "15",
         "ratingCount": "4",
-        "description": "hotel description 5"
-        , "address": "Tumkur"
-    , "latitude":13.3282036, "longitude":77.1107599 },   
+        "description": "hotel description 5",
+        "address": "Tumkur",
+        "latitude": 13.3282036,
+        "longitude": 77.1107599
+    },
     {
         "Id": "C1",
-        "name": 'cinima 1',
+        "name": "cinima 1",
         "category": "cinima",
         "imageUrl": "System",
         "reviewCount": "15",
         "ratingCount": "4",
-        "description": "cinima description 5"
-        , "address": "Tumkur"
-    , "latitude":13.3282036, "longitude":77.1107599 },   
+        "description": "cinima description 5",
+        "address": "Tumkur",
+        "latitude": 13.3282036,
+        "longitude": 77.1107599
+    },
     {
         "Id": "C2",
-        "name": 'cinima 2',
+        "name": "cinima 2",
         "category": "cinima",
         "imageUrl": "System",
         "reviewCount": "15",
         "ratingCount": "4",
-        "description": "cinima description 5"
-        , "address": "Tumkur"
-    , "latitude":13.3282036, "longitude":77.1107599 },   
+        "description": "cinima description 5",
+        "address": "Tumkur",
+        "latitude": 13.3282036,
+        "longitude": 77.1107599
+    },
     {
         "Id": "C3",
-        "name": 'cinima 3',
+        "name": "cinima 3",
         "category": "cinima",
         "imageUrl": "System",
         "reviewCount": "15",
         "ratingCount": "4",
-        "description": "cinima description 5"
-        , "address": "Tumkur"
-    , "latitude":13.3282036, "longitude":77.1107599 },   
+        "description": "cinima description 5",
+        "address": "Tumkur",
+        "latitude": 13.3282036,
+        "longitude": 77.1107599
+    },
     {
         "Id": "C4",
-        "name": 'cinima 4',
+        "name": "cinima 4",
         "category": "cinima",
         "imageUrl": "System",
         "reviewCount": "15",
         "ratingCount": "4",
-        "description": "cinima description 5"
-        , "address": "Tumkur"
-    , "latitude":13.3282036, "longitude":77.1107599 },   
+        "description": "cinima description 5",
+        "address": "Tumkur",
+        "latitude": 13.3282036,
+        "longitude": 77.1107599
+    },
     {
         "Id": "C5",
-        "name": 'cinima 5',
+        "name": "cinima 5",
         "category": "cinima",
         "imageUrl": "System",
         "reviewCount": "15",
         "ratingCount": "4",
-        "description": "cinima description 5"
-        , "address": "Tumkur"
-    , "latitude":13.3282036, "longitude":77.1107599 },   
+        "description": "cinima description 5",
+        "address": "Tumkur",
+        "latitude": 13.3282036,
+        "longitude": 77.1107599
+    },
     {
         "Id": "F1",
-        "name": 'flower 1',
+        "name": "flower 1",
         "category": "flower",
         "imageUrl": "System",
         "reviewCount": "15",
         "ratingCount": "4",
-        "description": "flower description 5"
-        , "address": "Tumkur"
-    , "latitude":13.3282036, "longitude":77.1107599 },   
+        "description": "flower description 5",
+        "address": "Tumkur",
+        "latitude": 13.3282036,
+        "longitude": 77.1107599
+    },
     {
         "Id": "F2",
-        "name": 'flower 2',
+        "name": "flower 2",
         "category": "flower",
         "imageUrl": "System",
         "reviewCount": "15",
         "ratingCount": "4",
-        "description": "flower description 5"
-        , "address": "Tumkur"
-    , "latitude":13.3282036, "longitude":77.1107599 },   
+        "description": "flower description 5",
+        "address": "Tumkur",
+        "latitude": 13.3282036,
+        "longitude": 77.1107599
+    },
     {
         "Id": "F3",
-        "name": 'flower 3',
+        "name": "flower 3",
         "category": "flower",
         "imageUrl": "System",
         "reviewCount": "15",
         "ratingCount": "4",
-        "description": "flower description 5"
-        , "address": "Tumkur"
-    , "latitude":13.3282036, "longitude":77.1107599 },   
+        "description": "flower description 5",
+        "address": "Tumkur",
+        "latitude": 13.3282036,
+        "longitude": 77.1107599
+    },
     {
         "Id": "F4",
-        "name": 'flower 4',
+        "name": "flower 4",
         "category": "flower",
         "imageUrl": "System",
         "reviewCount": "15",
         "ratingCount": "4",
-        "description": "flower description 5"
-        , "address": "Tumkur"
-    , "latitude":13.3282036, "longitude":77.1107599 },   
+        "description": "flower description 5",
+        "address": "Tumkur",
+        "latitude": 13.3282036,
+        "longitude": 77.1107599
+    },
     {
         "Id": "F5",
-        "name": 'flower 5',
+        "name": "flower 5",
         "category": "flower",
         "imageUrl": "System",
         "reviewCount": "15",
         "ratingCount": "4",
-        "description": "flower description 5"
-        , "address": "Tumkur"
-    , "latitude":13.3282036, "longitude":77.1107599 },   
+        "description": "flower description 5",
+        "address": "Tumkur",
+        "latitude": 13.3282036,
+        "longitude": 77.1107599
+    },
     {
         "Id": "D1",
-        "name": 'doctor 1',
+        "name": "doctor 1",
         "category": "doctor",
         "imageUrl": "System",
         "reviewCount": "15",
         "ratingCount": "4",
-        "description": "doctor description 5"
-        , "address": "Tumkur"
-    , "latitude":13.3282036, "longitude":77.1107599 },   
+        "description": "doctor description 5",
+        "address": "Tumkur",
+        "latitude": 13.3282036,
+        "longitude": 77.1107599
+    },
     {
         "Id": "D2",
-        "name": 'doctor 2',
+        "name": "doctor 2",
         "category": "doctor",
         "imageUrl": "System",
         "reviewCount": "15",
         "ratingCount": "4",
-        "description": "doctor description 5"
-        , "address": "Tumkur"
-    , "latitude":13.3282036, "longitude":77.1107599 },   
+        "description": "doctor description 5",
+        "address": "Tumkur",
+        "latitude": 13.3282036,
+        "longitude": 77.1107599
+    },
     {
         "Id": "D3",
-        "name": 'doctor 3',
+        "name": "doctor 3",
         "category": "doctor",
         "imageUrl": "System",
         "reviewCount": "15",
         "ratingCount": "4",
-        "description": "doctor description 5"
-        , "address": "Tumkur"
-    , "latitude":13.3282036, "longitude":77.1107599 },   
+        "description": "doctor description 5",
+        "address": "Tumkur",
+        "latitude": 13.3282036,
+        "longitude": 77.1107599
+    },
     {
         "Id": "D4",
-        "name": 'doctor 4',
+        "name": "doctor 4",
         "category": "doctor",
         "imageUrl": "System",
         "reviewCount": "15",
         "ratingCount": "4",
-        "description": "doctor description 5"
-        , "address": "Tumkur"
-    , "latitude":13.3282036, "longitude":77.1107599 },   
+        "description": "doctor description 5",
+        "address": "Tumkur",
+        "latitude": 13.3282036,
+        "longitude": 77.1107599
+    },
     {
         "Id": "D5",
-        "name": 'doctor 5',
+        "name": "doctor 5",
         "category": "doctor",
         "imageUrl": "System",
         "reviewCount": "15",
         "ratingCount": "4",
-        "description": "doctor description 5"
-        , "address": "Tumkur"
-    , "latitude":13.3282036, "longitude":77.1107599 },   
+        "description": "doctor description 5",
+        "address": "Tumkur",
+        "latitude": 13.3282036,
+        "longitude": 77.1107599
+    },
     {
         "Id": "P1",
-        "name": 'pharma 1',
+        "name": "pharma 1",
         "category": "pharma",
         "imageUrl": "System",
         "reviewCount": "15",
         "ratingCount": "4",
-        "description": "pharma description 5"
-        , "address": "Tumkur"
-    , "latitude":13.3282036, "longitude":77.1107599 },   
+        "description": "pharma description 5",
+        "address": "Tumkur",
+        "latitude": 13.3282036,
+        "longitude": 77.1107599
+    },
     {
         "Id": "P2",
-        "name": 'pharma 2',
+        "name": "pharma 2",
         "category": "pharma",
         "imageUrl": "System",
         "reviewCount": "15",
         "ratingCount": "4",
-        "description": "pharma description 5"
-        , "address": "Tumkur"
-    , "latitude":13.3282036, "longitude":77.1107599 },   
+        "description": "pharma description 5",
+        "address": "Tumkur",
+        "latitude": 13.3282036,
+        "longitude": 77.1107599
+    },
     {
         "Id": "P3",
-        "name": 'pharma 3',
+        "name": "pharma 3",
         "category": "pharma",
         "imageUrl": "System",
         "reviewCount": "15",
         "ratingCount": "4",
-        "description": "pharma description 5"
-        , "address": "Tumkur"
-    , "latitude":13.3282036, "longitude":77.1107599 },   
+        "description": "pharma description 5",
+        "address": "Tumkur",
+        "latitude": 13.3282036,
+        "longitude": 77.1107599
+    },
     {
         "Id": "P4",
-        "name": 'pharma 4',
+        "name": "pharma 4",
         "category": "pharma",
         "imageUrl": "System",
         "reviewCount": "15",
         "ratingCount": "4",
-        "description": "pharma description 5"
-        , "address": "Tumkur"
-    , "latitude":13.3282036, "longitude":77.1107599 },   
+        "description": "pharma description 5",
+        "address": "Tumkur",
+        "latitude": 13.3282036,
+        "longitude": 77.1107599
+    },
     {
         "Id": "P5",
-        "name": 'pharma 5',
+        "name": "pharma 5",
         "category": "pharma",
         "imageUrl": "System",
         "reviewCount": "15",
