b6065ac77b87015f564b89d93fc54e327ededb99
vinaykempa <vinay.madenahallyprasad@6dtech.co.in>
2017-03-21 01:01:26 +0530

    Dash changes

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -22,31 +22,31 @@ export class DashboardComponent implements OnInit {
       "cardName": "Hotels",
       "routeLink": "/list/hotel",
       "iconClass": "fa fa-h-square",
-      "imageUrl": "../../assets/images/IMG-20170201-WA0030.jpg"
+      "imageUrl": "../../assets/images/IMG-20170201-WA0021.jpg"
     },
     {
       "cardName": "Cinima",
       "routeLink": "/list/cinima",
       "iconClass": "fa fa-film",
-      "imageUrl": "../../assets/images/IMG-20170201-WA0030.jpg"
+      "imageUrl": "../../assets/images/IMG-20170201-WA0025.jpg"
     },
     {
       "cardName": "flower shops",
       "routeLink": "/list/flower",
       "iconClass": "fa fa-gift",
-      "imageUrl": "../../assets/images/IMG-20170201-WA0030.jpg"
+      "imageUrl": "../../assets/images/IMG-20170201-WA0012.jpg"
     },
     {
       "cardName": "doctors",
       "routeLink": "/list/doctor",
       "iconClass": "fa fa-user-md",
-      "imageUrl": "../../assets/images/IMG-20170201-WA0030.jpg"
+      "imageUrl": "../../assets/images/aba982dce0439a0d8332805300ec3880.jpg"
     },
     {
       "cardName": "pharma",
       "routeLink": "/list/pharma",
       "iconClass": "fa fa-plus-square",
-      "imageUrl": "../../assets/images/IMG-20170201-WA0030.jpg"
+      "imageUrl": "../../assets/images/Compo_Pharma_Fingerprint_Fotolia_270x180.jpg"
     }];
 
   ngOnInit() {
