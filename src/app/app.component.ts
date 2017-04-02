b5d2c7ee49132649b7f2337c03373debbf3fd7e3
Amith <amith.kumar@gmail.com>
2017-04-02 13:17:54 +0530

    Hiding Navbar onlu in home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'] //'./component/dashboard/dashboard.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+constructor(private route: ActivatedRoute,
+private router: Router) { 
+ 
+}
+
+ngOnInit(){
+}
 
 }
+
+  
